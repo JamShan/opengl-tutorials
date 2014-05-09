fe199162053e5ac88d2c0b901a71383c793f701e
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2014-05-09 19:50:29 +0200

    Add note about flipping the TGA.

--- ports/opengl-tutorial.org/samples/tut05/tut_05_textured_cube.d
+++ ports/opengl-tutorial.org/samples/tut05/tut_05_textured_cube.d
@@ -296,6 +296,10 @@ private:
     // This isn't efficient but it serves as an example.
     float[] uvArr =
     [
+        // Note: the '1.0f -' part is there in case the .tga image
+        // was already flipped. The texture loading routines in
+        // glamour may or may not flip the TGA vertically,
+        // based on the code path it takes.
         0.000059f, 1.0f - 0.000004f,
         0.000103f, 1.0f - 0.336048f,
         0.335973f, 1.0f - 0.335903f,
